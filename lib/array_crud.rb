f9df0468d4c866053c0fb34ed39010d39aecf90a
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-05 00:07:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ["Now or Never", "What Lovers Do", "No Tears Left To Cry", "Eastside"]
 end
 
 def add_element_to_end_of_array(array, element)
